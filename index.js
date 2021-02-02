6ade8fce36d2aa943f665fa88189c8971d145a1c
Vishal Nirmal <vishalnirmal53@gmail.com>
2021-02-02 14:42:16 +0530

    set the default theme to dark mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var body = document.querySelector("body");
 var chechkbox = document.querySelector(".checkbox");
 var toggle_button = document.querySelector(".toggle-button");
 chechkbox.addEventListener("input", ()=>{
-    if(chechkbox.checked)
+    if(!chechkbox.checked)
         body.classList.add("dark");
     else
         body.classList.remove("dark");
