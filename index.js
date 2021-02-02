ad039487960a888bb1a7e35234c059b5ab8958d0
Vishal Nirmal <vishalnirmal53@gmail.com>
2021-02-02 13:33:20 +0530

    Added styling to the component for desktop version

---
+++ index.js
@@ -0,0 +1,9 @@
+var body = document.querySelector("body");
+var chechkbox = document.querySelector(".checkbox");
+var toggle_button = document.querySelector(".toggle-button");
+chechkbox.addEventListener("input", ()=>{
+    if(chechkbox.checked)
+        body.classList.add("dark");
+    else
+        body.classList.remove("dark");
+});
